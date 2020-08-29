0957db5652e96fb037ea8f63bbb01e6fdbf3ac47
Radek Tomasek <radektomasek@Radeks-MacBook-Pro.local>
2020-08-29 16:46:56 +0100

    Added new route

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const port = 3000;
 
 app.get('/', (request, response) => {
   response.send('Hello World!');
-})
+});
+
+app.get('/demo', (request, response) => {
+  response.set('X-full-stack', '4life');
+  response.status(418);
+  response.send('I prefer coffee');
+});
 
 app.listen(port, () => {
-  console.log(`Example App listening on port ${port}`)
-})
+  console.log(`Example App listening on port ${port}`);
+});
